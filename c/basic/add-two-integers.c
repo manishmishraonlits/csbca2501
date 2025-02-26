9d2e06394e607df717fc11918f6b69238c20a611
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2025-02-26 16:25:15 +0530

    20250224

---
+++ c/basic/add-two-integers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+int a, b, c;
+printf("Program to add two integers\n\n");
+printf("Enter a number: ");
+scanf("%d", &a);
+printf("Enter another number: ");
+scanf("%d", &b);
+c = a + b
+printf("The sum is %d.", c);
+return 0;
+}
---
+++ c/basic/area-rectangle.c
@@ -0,0 +1,6 @@
+int main(void)
+{
+    scanf("%d%d", &l, &b);
+    a = l * b;
+    printf("%d", a);
+}
