b1d350ded0ded75c95e2f28d238f307089d07cc4
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2025-04-21 13:51:17 +0530

    20250421

---
+++ c/user-defined-function/swap.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int swap(int a, int b);
+
+// void swap(void); // Category 1
+// void swap(int a, int b) //Category 4
+// int swap(int a, int b) //Category 3
+// int swap(void); //Category 4
